3b405cb1f6cd0d247ec344488cb58de8c42161ba
quitran1303 <quitran1303@gmail.com>
2023-10-05 09:35:31 +1100

    new commit for branch

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
 	printf("Hello world, 1st checkin!");
     printf("add code for test git workflows 2nd");
+    printf("add new line for pull request");
 
 	return 0;
 }
