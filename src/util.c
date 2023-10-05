f50aee003a1e19d2fb2d3781ea6e3c42df7ad231
quitran1303 <quitran1303@gmail.com>
2023-10-05 11:09:36 +1100

    add function ltrim for passing the test

--- src/util.c
+++ src/util.c
@@ -68,3 +68,19 @@ char* readline() {
 
     return data;
 }
+
+char* ltrim(char* str){
+    if (!str){
+        return '\0';
+    }
+
+    if (!*str){
+        return str;
+    }
+
+    while(*str != '\0' && isspace(*str)){
+        str++;
+    }
+
+    return str;
+}
